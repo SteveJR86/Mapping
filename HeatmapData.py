53917b95bad004aa8a29e4eaf1a4567c30602009
SteveJR86 <15844719+SteveJR86@users.noreply.github.com>
2021-09-15 13:02:21 +0100

    heatmapdata working properly

---
+++ HeatmapData.py
@@ -0,0 +1,69 @@
+import xlsxwriter
+from datetime import date
+import upland
+import os
+
+headers = {'user-agent': 'HeatMap/1.1'}
+homedir = os.path.expanduser('~')
+
+searchCity = 'Chicago'
+tableData = {}
+
+neighbourhoods = upland.getNeighbourhood(headers, searchCity)
+
+for neighbourhood in neighbourhoods:
+  tableData[neighbourhood['id']] = {'neighbourhood_name': neighbourhood['name'], 'coordinates': neighbourhood['boundaries']['coordinates'][0], 'data':{'total_properties': 0, 'total_unlocked_properties': 0, 'total_minted': 0, 'owned_properties': 0, 'non_fsa_unminted_properties': 0, 'fsa_unminted_properties': 0, 'locked_properties': 0, 'for_sale_properties': 0}}
+
+for neighbourhoodID, details in tableData.items():
+  props = upland.getNeighbourhoodProperties(headers, searchCity, details['neighbourhood_name'])
+  tableData[neighbourhoodID]['data']['total_properties'] = len(props[0])
+  tableData[neighbourhoodID]['data']['total_unlocked_properties'] = len(props[0])
+  for prop in props[0]:
+    if prop['status'] == 'Owned':
+      tableData[neighbourhoodID]['data']['owned_properties'] += 1
+      tableData[neighbourhoodID]['data']['total_minted'] += 1
+    elif prop['status'] == 'For sale':
+      tableData[neighbourhoodID]['data']['for_sale_properties'] += 1
+      tableData[neighbourhoodID]['data']['total_minted'] += 1
+    elif prop['status'] == 'Unlocked':
+      if prop['labels']['fsa_allow']:
+        tableData[neighbourhoodID]['data']['fsa_unminted_properties'] += 1
+      else:
+        tableData[neighbourhoodID]['data']['non_fsa_unminted_properties'] += 1
+    elif prop['status'] == 'Locked':
+      tableData[neighbourhoodID]['data']['locked_properties'] += 1
+      tableData[neighbourhoodID]['data']['total_unlocked_properties'] -= 1
+
+today = date.today()
+workbook = xlsxwriter.Workbook(homedir + '/maps/HeatmapData/' + searchCity + ' ' + today.strftime('%d-%b') + '.xlsx')
+worksheet = workbook.add_worksheet()
+percentage_format = workbook.add_format()
+percentage_format.set_num_format('0%')
+row = 0
+col = 0
+
+firstNeighbourhoodID = next(iter(tableData))
+worksheet.write(row, col, 'neighbourhood_name')
+
+for columnHeader in tableData[firstNeighbourhoodID]['data']:
+  col += 1
+  worksheet.write(row, col, columnHeader)
+  if not col == 1:
+    col += 1
+    worksheet.write(row, col, '% ' + columnHeader)
+row += 1
+
+for neighbourhoodData in tableData.values():
+  col = 0
+  worksheet.write(row, col, neighbourhoodData['neighbourhood_name'])
+  for data in neighbourhoodData['data'].values():
+    col += 1
+    worksheet.write(row, col, data)
+    if not col == 1:
+      col += 1
+      worksheet.write(row, col, '=INDIRECT("R[0]C[-1]", FALSE())/INDIRECT("R[0]C[-' + str(col-1) + ']", FALSE() * 100)', percentage_format)
+  row += 1
+
+workbook.close()
+    
+            
---
+++ mappingDevelopment.py
@@ -0,0 +1,72 @@
+import csv
+import os
+from time import sleep
+import cairo
+import json
+import requests
+import xlsxwriter
+from shapely.geometry import Point
+from shapely.geometry import Polygon
+from datetime import date
+import upland
+
+def main():
+  headers = {'user-agent': 'MapApp/1.1'}
+  homedir = os.path.expanduser('~')
+  # parameters to set up depending on what is required:
+  # Mode should be set to 1 for Neighbourhood plot, 2 for Area plot
+  # and 3 for Street plot
+
+  mapHeight = 3000
+  propData = {}
+  neighbourhood = "Woodward Park"
+  city = "Fresno"
+  filename = homedir + '/maps/' + neighbourhood + " - " + city + " - Building Progress"
+  
+  properties = upland.getNeighbourhoodProperties(headers, city, neighbourhood)
+  neighbourhoodPoly = upland.getNeighbourhoodPoly(headers, city, neighbourhood)
+  props = properties[0]   
+  
+  canvas2 = upland.makeCanvas(neighbourhoodPoly)
+  surface = canvas2[0]
+  canvas = canvas2[1]
+  mapFactor = canvas2[2]
+  minLat = canvas2[3]
+  minLong = canvas2[4]
+  neighbourhoodPoly = neighbourhoodPoly[0]
+  
+  for prop in props:
+    coords = json.loads(prop['boundaries'])
+    centrePoint = Point(float(prop['centerlng']), float(prop['centerlat']))
+    if centrePoint.within(neighbourhoodPoly):
+      for num, point in enumerate(coords['coordinates'][0]):
+        if num == len(coords['coordinates'][0]) - 1:
+          break
+        if num == 0:
+          canvas.move_to(((point[0] - neighbourhoodPoly.bounds[0]) * mapFactor), (mapHeight - (point[1] - neighbourhoodPoly.bounds[1]) * mapFactor))
+        else:
+          canvas.line_to(((point[0] - neighbourhoodPoly.bounds[0]) * mapFactor), (mapHeight - (point[1] - neighbourhoodPoly.bounds[1]) * mapFactor))
+      if prop['status'] == 'Owned':
+        try:
+          propDetails = json.loads(requests.get('https://api.upland.me/properties/' + str(prop['prop_id']), headers=headers).text)
+        except:
+          sleep(1)
+          propDetails = json.loads(requests.get('https://api.upland.me/properties/' + str(prop['prop_id']), headers=headers).text)
+        if propDetails['building'] == None:
+          canvas.set_source_rgb(1, 1, 1)
+        elif propDetails['building']['constructionStatus'] == 'completed':
+          canvas.set_source_rgb(0, 1, 0.15)
+        elif propDetails['building']['constructionStatus'] == 'processing' or propDetails['building']['constructionStatus'] == 'can-watch-ceremony':
+          canvas.set_source_rgb(1, 1, 0)
+      else:
+        canvas.set_source_rgb(1, 1, 1)
+      canvas.close_path()
+      canvas.fill_preserve()
+      canvas.set_source_rgb(0, 0, 0)
+      canvas.stroke()
+
+  today = date.today()
+  surface.write_to_png(filename + ' ' + today.strftime('%d-%b') + '.png')
+
+if __name__ == '__main__':
+    main()
