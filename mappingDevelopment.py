a5fcc3dbcc547b844da7256bd4d8a9924817e689
SteveJR86 <15844719+SteveJR86@users.noreply.github.com>
2021-09-16 08:48:02 +0100

    mappingDevelopment.py updated to use plotObject function in Upland module rather than plotting directly within this file

--- mappingDevelopment.py
+++ mappingDevelopment.py
@@ -19,8 +19,8 @@ def main():
   propData = {}
   neighbourhood = sys.argv[1]
   city = sys.argv[2]
-#  neighbourhood = "midtown terrace"
-#  city = "San Francisco"
+##  neighbourhood = "midtown terrace"
+##  city = "San Francisco"
   filename = homedir + '/maps/' + neighbourhood + " - " + city + " - Building Progress"
 
   props = upland.getNeighbourhoodProperties(headers, city, neighbourhood)
@@ -30,42 +30,30 @@ def main():
   canvas = canvas2[1]
   mapFactor = canvas2[2]
   minLat = canvas2[3]
-  minLong = canvas2[4]
+  maxLong = canvas2[4]
   neighbourhoodPoly = neighbourhoodPoly[0]
 
   builtProps = 0
   inProgressProps = 0
   for prop in props:
-    coords = json.loads(prop['boundaries'])
-    centrePoint = Point(float(prop['centerlng']), float(prop['centerlat']))
-    if centrePoint.within(neighbourhoodPoly):
-      for num, point in enumerate(coords['coordinates'][0]):
-        if num == len(coords['coordinates'][0]) - 1:
-          break
-        if num == 0:
-          canvas.move_to(((point[0] - neighbourhoodPoly.bounds[0]) * mapFactor), (mapHeight - (point[1] - neighbourhoodPoly.bounds[1]) * mapFactor))
-        else:
-          canvas.line_to(((point[0] - neighbourhoodPoly.bounds[0]) * mapFactor), (mapHeight - (point[1] - neighbourhoodPoly.bounds[1]) * mapFactor))
-      if prop['status'] == 'Owned':
-        try:
-          propDetails = json.loads(requests.get('https://api.upland.me/properties/' + str(prop['prop_id']), headers=headers).text)
-        except:
-          sleep(1)
-          propDetails = json.loads(requests.get('https://api.upland.me/properties/' + str(prop['prop_id']), headers=headers).text)
-        if propDetails['building'] == None:
-          canvas.set_source_rgb(1, 1, 1)
-        elif propDetails['building']['constructionStatus'] == 'completed':
-          canvas.set_source_rgb(0, 1, 0.15)
-          builtProps += 1
-        elif propDetails['building']['constructionStatus'] == 'processing' or propDetails['building']['constructionStatus'] == 'can-watch-ceremony':
-          canvas.set_source_rgb(1, 1, 0)
-          inProgressProps += 1
-      else:
-        canvas.set_source_rgb(1, 1, 1)
-      canvas.close_path()
-      canvas.fill_preserve()
-      canvas.set_source_rgb(0, 0, 0)
-      canvas.stroke()
+    propBound = json.loads(prop['boundaries'])
+    propPoly = Polygon(propBound['coordinates'][0])
+    if prop['status'] == 'Owned':
+      try:
+        propDetails = json.loads(requests.get('https://api.upland.me/properties/' + str(prop['prop_id']), headers=headers).text)
+      except:
+        sleep(1)
+        propDetails = json.loads(requests.get('https://api.upland.me/properties/' + str(prop['prop_id']), headers=headers).text)
+      if propDetails['building'] == None:
+        upland.plotObject(canvas, mapFactor, propPoly, minLat, maxLong, (1, 1, 1))
+      elif propDetails['building']['constructionStatus'] == 'completed':
+        upland.plotObject(canvas, mapFactor, propPoly, minLat, maxLong, (0, 1, 0.15))
+        builtProps += 1
+      elif propDetails['building']['constructionStatus'] == 'processing' or propDetails['building']['constructionStatus'] == 'can-watch-ceremony':
+        upland.plotObject(canvas, mapFactor, propPoly, minLat, maxLong, (1, 1, 0))
+        inProgressProps += 1
+    else:
+      upland.plotObject(canvas, mapFactor, propPoly, minLat, maxLong, (1, 1, 1))
   canvas.set_font_size(100)
   canvas.move_to(75, 75)
   canvas.show_text(f'{neighbourhood}, {city}: {(builtProps/len(props))*100:.0f}% Developed')
