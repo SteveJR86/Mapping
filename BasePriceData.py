2faf509eaae288bb25c8a03d01e063fcaa26c367
SteveJR86 <15844719+SteveJR86@users.noreply.github.com>
2021-09-23 22:10:43 +0100

    added basepricedata and prereleasedata scripts

---
+++ BasePriceData.py
@@ -0,0 +1,79 @@
+import xlsxwriter
+from datetime import date
+import Upland.upland as upland
+import os
+import sys
+
+def main():
+
+  headers = {'user-agent': 'BasePrice/1.1'}
+  homedir = os.path.expanduser('~')
+
+  searchCity = sys.argv[1]
+  tableData = {}
+
+  neighbourhoods = upland.getNeighbourhood(headers, searchCity)
+
+  for neighbourhood in neighbourhoods:
+    tableData[neighbourhood['id']] = {'neighbourhood name': neighbourhood['name'], 'data':{'base price': 0}}
+
+  for neighbourhoodID, details in tableData.items():
+    props = upland.getNeighbourhoodProperties(headers, searchCity, details['neighbourhood name'])
+    if not len(props) == 0:
+      for prop in props:
+        if not prop['status'] == 'Locked':
+          break
+      propDetails = upland.getPropertyDetails(headers, prop['prop_id'])
+      tableData[neighbourhoodID]['data']['base price'] = calculateBasePrice(headers, propDetails)
+
+  today = date.today()
+  workbook = xlsxwriter.Workbook(homedir + '/maps/HeatmapData/' + searchCity + ' Base Prices.xlsx')
+  worksheet = workbook.add_worksheet()
+  percentage_format = workbook.add_format()
+  percentage_format.set_num_format('0%')
+  row = 0
+  col = 0
+
+  firstNeighbourhoodID = next(iter(tableData))
+  worksheet.write(row, col, 'neighbourhood name')
+
+  for columnHeader in tableData[firstNeighbourhoodID]['data']:
+    col += 1
+    worksheet.write(row, col, columnHeader)
+  row += 1
+
+  for neighbourhoodData in tableData.values():
+    col = 0
+    worksheet.write(row, col, neighbourhoodData['neighbourhood name'])
+    for data in neighbourhoodData['data'].values():
+      col += 1
+      worksheet.write(row, col, data)
+    row += 1
+
+  workbook.close()
+
+  return
+    
+def calculateBasePrice(headers, prop):
+  try:
+    price = (prop['yield_per_hour']*50000)/prop['area']
+  except:
+    price = 0
+  collectionLevels = []
+  collections = upland.matchCollections(headers, prop['prop_id'])
+  for collection in collections:
+    collectionLevels.append(collection['category'])
+  if max(collectionLevels) == 5:
+    basePrice = price/21
+  elif max(collectionLevels) == 4:
+    basePrice = price/11
+  elif max(collectionLevels) == 3:
+    basePrice = price/3
+  elif max(collectionLevels) == 2:
+    basePrice = price/1.5
+  else:
+    basePrice = price
+  return basePrice
+
+if __name__ == '__main__':
+  main()
---
+++ PreReleaseData.py
@@ -0,0 +1,51 @@
+import xlsxwriter
+from datetime import date
+import Upland.upland as upland
+import os
+import sys
+
+headers = {'user-agent': 'HeatMap/1.1'}
+homedir = os.path.expanduser('~')
+
+searchCity = sys.argv[1]
+tableData = {}
+
+neighbourhoods = upland.getNeighbourhood(headers, searchCity)
+
+for neighbourhood in neighbourhoods:
+  tableData[neighbourhood['id']] = {'neighbourhood name': neighbourhood['name'], 'data':{'total properties': 0, 'non-fsa properties': 0, 'fsa properties': 0}}
+
+for neighbourhoodID, details in tableData.items():
+  props = upland.getNeighbourhoodProperties(headers, searchCity, details['neighbourhood name'])
+  tableData[neighbourhoodID]['data']['total properties'] = len(props)
+  for prop in props:
+    if prop['labels']['fsa_allow']:
+      tableData[neighbourhoodID]['data']['fsa properties'] += 1
+    else:
+      tableData[neighbourhoodID]['data']['non-fsa properties'] += 1
+
+today = date.today()
+workbook = xlsxwriter.Workbook(homedir + '/maps/HeatmapData/' + searchCity + ' Pre-Release Data' + today.strftime('%d-%b') + '.xlsx')
+worksheet = workbook.add_worksheet()
+row = 0
+col = 0
+
+firstNeighbourhoodID = next(iter(tableData))
+worksheet.write(row, col, 'neighbourhood name')
+
+for columnHeader in tableData[firstNeighbourhoodID]['data']:
+  col += 1
+  worksheet.write(row, col, columnHeader)
+row += 1
+
+for neighbourhoodData in tableData.values():
+  col = 0
+  worksheet.write(row, col, neighbourhoodData['neighbourhood name'])
+  for data in neighbourhoodData['data'].values():
+    col += 1
+    worksheet.write(row, col, data)
+  row += 1
+
+workbook.close()
+    
+            
