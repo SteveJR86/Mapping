f141e6811b0c84edf0205adebdab2625b49f2ba2
SteveJR86 <15844719+SteveJR86@users.noreply.github.com>
2022-02-27 09:48:38 +0000

    buildfit now includes spreadsheet output

--- BuildFit.py
+++ BuildFit.py
@@ -2,50 +2,47 @@ import Upland.upland as upland
 import Upland.plotting as plotting
 from shapely import affinity
 import math
-
-def checkFit(headers, propID):
-
-  scale = 111139
-  
-  propDetails = upland.getPropertyDetails(headers, propID)
-  propPoly = upland.makePoly(propDetails['boundaries'])
-  propPolyNorm = affinity.affine_transform(propPoly, [scale, 0, 0, scale, (-propPoly.bounds[0]*scale), (-propPoly.bounds[1]*scale)])
-  hyp1 = math.sqrt(((propPolyNorm.exterior.coords[1][0]-propPolyNorm.exterior.coords[0][0])**2)+((propPolyNorm.exterior.coords[1][1]-propPolyNorm.exterior.coords[0][1])**2))
-  hyp2 = math.sqrt(((propPolyNorm.exterior.coords[2][0]-propPolyNorm.exterior.coords[1][0])**2)+((propPolyNorm.exterior.coords[2][1]-propPolyNorm.exterior.coords[1][1])**2))
-  if hyp1 > hyp2:
-    opp = propPolyNorm.exterior.coords[1][1] - propPolyNorm.exterior.coords[0][1]
-    adj = propPolyNorm.exterior.coords[0][0] - propPolyNorm.exterior.coords[1][0]
-  else:
-    opp = propPolyNorm.exterior.coords[2][1] - propPolyNorm.exterior.coords[1][1]
-    adj = propPolyNorm.exterior.coords[1][0] - propPolyNorm.exterior.coords[2][0]
-  angle = math.degrees(math.atan(opp/adj))
-  propPolyRot = affinity.rotate(propPolyNorm, 90+angle)
-
-  buildPossible = {}
-  buildTypes = upland.getBuildings(headers, propID)
-  for buildType in buildTypes:
-    buildPoly = upland.makePoly({'type': 'Polygon', 'coordinates': [buildType['boundaries']]})
-    buildPolyNorm = affinity.affine_transform(buildPoly, [3, 0, 0, 3, (-buildPoly.bounds[0]*3), (-buildPoly.bounds[1]*3)])
-    
-    buildPolyCentered = affinity.affine_transform(buildPolyNorm, [1, 0, 0, 1, -(buildPolyNorm.centroid.coords[0][0] - propPolyRot.centroid.coords[0][0]), -(buildPolyNorm.centroid.coords[0][1] - propPolyRot.centroid.coords[0][1])])
-    if buildPolyCentered.within(propPolyRot):
-      buildPossible[buildType['buildingImage']] = True
-    else:
-      buildPossible[buildType['buildingImage']] = False
-
-  return buildPossible
+import xlsxwriter
+import os
 
 def main():
   headers = {'user-agent': 'buildfit/1.0'}
-  propID = 81450433868377
+  neighbourhood = 'Avalon Park'
+  city = 'Chicago'
+  homedir = os.path.expanduser('~')
+  workbook = xlsxwriter.Workbook(homedir + '/maps/BuildFitData/' + city + ' - ' + neighbourhood + '.xlsx')
+  worksheet = workbook.add_worksheet()
+  props = upland.getNeighbourhoodProperties(headers, city, neighbourhood)
+  buildTypes = upland.getBuildings(headers, props[0][0]['prop_id'])
+  worksheet.write(0, 0, 'Property Link')
+  worksheet.write(0, 1, 'Full Address')
+  worksheet.write(0, 2, 'Username')
+  worksheet.write(0, 3, 'Is It Built?')
+  col = 4
+  for buildType in buildTypes:
+    worksheet.write(0, col, buildType['buildingImage'].split("/")[0].replace("_", " ").replace(" new", "").replace("2", " 2"))
+    col += 1
+  col = 4
+  row = 1
+  for prop in props[0]:
+    response, propDetails = upland.checkFit(headers, prop['prop_id'])
+    worksheet.write(row, 0, f"https://play.upland.me/?prop_id={propDetails['prop_id']}")
+    worksheet.write(row, 1, propDetails['full_address'])
+    worksheet.write(row, 2, propDetails['owner_username'])
+    try:
+      worksheet.write(row, 3, propDetails['building']['constructionStatus'])
+    except:
+      pass
+    for x, y in response.items():
+      if y:
+        worksheet.write(row, col, 'Fits')
+      else:
+        worksheet.write(row, col, 'Does Not Fit')
+      col += 1
+    row += 1
+    col = 4
+  workbook.close()
 
-  response = checkFit(headers, propID)
-  for x, y in response.items():
-    if y:
-      print(f'Building type {x.split("/")[0].replace("_", " ").replace(" new", "").replace("2", " 2")} will fit')
-    else:
-      print(f'Building type {x.split("/")[0].replace("_", " ").replace(" new", "").replace("2", " 2")} will not fit')
-  
   return
 
 if __name__ == '__main__':
